36c15c887002707cb94cbfeb7c3acaba60bb69e7
Juan Camilo Holguin <camiloholguin9605@gmail.com>
2026-02-14 19:15:07 -0500

    actualizando javascript y css

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ document.getElementById("contactForm").addEventListener("submit", function(e){
   e.preventDefault();
   alert("Gracias por tu mensaje. Te contactaremos pronto.");
 });
+
+window.addEventListener("scroll", function(){
+  const header = document.querySelector("header");
+  header.classList.toggle("scrolled", window.scrollY > 50);
+});
